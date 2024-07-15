6715c82a72a3744e707c3a44a8d74555dc950a20
hpharmsen <hp@harmsen.nl>
2024-07-15 10:37:51 +0200

    Fixes mbt Anthropic json output

--- justai/models/anthropic_models.py
+++ justai/models/anthropic_models.py
@@ -13,8 +13,8 @@ from justai.tools.display import ERROR_COLOR, color_print
 
 # Models:
 # Claude 3 Opus	    claude-3-opus-20240229
-# Claude 3 Sonnet	claude-3-sonnet-20240229
-# Claude 3 Haiku	Coming soon
+# Claude 3 Sonnet	claude-3-5-sonnet-20240620
+# Claude 3 Haiku	claude-3-haiku-20240307
 #
 # Supported parameters:
 # max_tokens: int (default 800)
@@ -56,7 +56,6 @@ class AnthropicModel(Model):
             -> tuple[[str | object], int, int]:
 
         antr_messages = self.transform_messages(messages, return_json)
-
         try:
             message = self.client.messages.create(
                 model=self.model_name,
@@ -68,9 +67,15 @@ class AnthropicModel(Model):
         except anthropic.InternalServerError as e:
             raise OverloadedException(e)
 
-        response = message.content[0].text
+        response_str = message.content[0].text
         if return_json:
-            response = json.loads('{' + response, strict=False)
+            response_str = response_str.split('</json>')[0]  #!!
+            try:
+                response = json.loads(response_str, strict=False)
+            except json.decoder.JSONDecodeError:
+                print('ERROR DECODING JSON, RESPONSE:', response_str)
+        else:
+            response = response_str
         input_tokens = message.usage.input_tokens
         output_tokens = message.usage.output_tokens
         return response, input_tokens, output_tokens
@@ -104,7 +109,7 @@ class AnthropicModel(Model):
         if msgs and msgs[0].role == 'assistant':
             msgs = msgs[1:]
         if msgs and return_json:
-            msgs += [Message('assistant', 'sure here\'s your json: {')]
+            msgs += [Message('assistant', '<json>')]
         result = [create_anthropic_message(msg) for msg in msgs]
         return result
 
